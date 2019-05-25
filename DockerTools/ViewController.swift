be8aee880c68c27374ecdfcef4667ab6ede461fb
lou-lan <loulan@loulan.me>
2019-05-25 14:45:20 +0800

    first commit

---
+++ DockerTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DockerTools
+//
+//  Created by 翟怀楼 on 2019/4/28.
+//  Copyright © 2019 翟怀楼. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
